545f05d1d7a67046b58490d67e06efd321ae7776
fabricio jacob <fabrluc@outlook.com>
2024-01-09 22:20:26 -0300

    add

--- src/main/java/com/github/fabrluc/practicespring/controller/PostController.java
+++ src/main/java/com/github/fabrluc/practicespring/controller/PostController.java
@@ -3,6 +3,7 @@ package com.github.fabrluc.practicespring.controller;
 import com.github.fabrluc.practicespring.dto.Post;
 import com.github.fabrluc.practicespring.service.JsonPlaceholderService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/posts")
 @RequiredArgsConstructor
@@ -19,6 +21,7 @@ public class PostController {
 
     @GetMapping
     public List<Post> findAll() {
+        log.info(Thread.currentThread().getName());
         return jsonPlaceholderService.findAll();
     }
 
