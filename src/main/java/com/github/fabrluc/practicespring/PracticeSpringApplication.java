c1f960a65ea9fc35a8b6aac247ce24d70cd91200
fabricio jacob <fabrluc@outlook.com>
2024-04-03 22:33:41 -0300

    add

--- src/main/java/com/github/fabrluc/practicespring/PracticeSpringApplication.java
+++ src/main/java/com/github/fabrluc/practicespring/PracticeSpringApplication.java
@@ -1,19 +1,9 @@
 package com.github.fabrluc.practicespring;
 
-import com.github.fabrluc.practicespring.entities.Product;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.transaction.annotation.Transactional;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-import java.io.Flushable;
-import java.math.BigDecimal;
-import java.util.Random;
 
 @SpringBootApplication
 @EnableAutoConfiguration
