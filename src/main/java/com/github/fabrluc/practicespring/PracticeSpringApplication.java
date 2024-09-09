2d3ed3fd57357f428cab2fef73c2b2e387807756
spark <fabricio.jacob@outlook.com>
2024-09-08 21:32:42 -0300

    add

--- src/main/java/com/github/fabrluc/practicespring/PracticeSpringApplication.java
+++ src/main/java/com/github/fabrluc/practicespring/PracticeSpringApplication.java
@@ -1,12 +1,10 @@
 package com.github.fabrluc.practicespring;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 @EnableTransactionManagement
 public class PracticeSpringApplication {
 
